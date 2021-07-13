9e56a3ccd501bb8b0ffd9d9178bc40a2fc29af06
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 18:38:18 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number%10;
+        if(lastDigit>5)
+        {
+            Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number,lastDigit);
+        }
+        else if(lastDigit==0)
+        {
+           Console.WriteLine("The last digit of {0} is {1} and is 0", number,lastDigit);
+        }
+        else Console.WriteLine("The last digit of {0} is {1} and is less than 6 ad not 0", number,lastDigit);
+	    
+    }
+}
