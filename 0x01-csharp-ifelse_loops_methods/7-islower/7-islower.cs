0b27def6fcb3f78cb7cff6aa151c90a4c70b0db1
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 20:37:46 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g' };
+            for ( int i = 0; i < letters.Length; i++)
+            {
+                if (isLow(letters[i]))
+                {
+                    Console.WriteLine("{0} is lowercase", letters[i]);
+                }
+                else Console.WriteLine("{0} is uppercase", letters[i]);
+            }
+
+        }
+        public static bool isLow(char c)
+        {
+            if (c >= 'a' && c <= 'z')
+                return true;
+            else return false;
+        }
+    }
+}
