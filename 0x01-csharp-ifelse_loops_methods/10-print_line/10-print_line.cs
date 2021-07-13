f740cbaffa54ded150c2a0b2dfb5d9114a86ee5a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 21:35:20 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printLine(3);
+            printLine(0);
+            printLine(12);
+            printLine(-98);
+        }
+
+        public static void printLine(int length)
+        {
+            if(length >0)
+            {
+                for(int i=0;i<length;i++)
+                {
+                    Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine();
+        }
+    }
+}
