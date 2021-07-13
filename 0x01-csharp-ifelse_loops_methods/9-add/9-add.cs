ce1ebea1322afc2972e2377bd60c32ed5d16b082
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 21:07:28 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _9_add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", addition(1, 2));
+            Console.WriteLine("{0}", addition(98, 0));
+            Console.WriteLine("{0}", addition(100, -2));
+        }
+
+        public static int addition(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
