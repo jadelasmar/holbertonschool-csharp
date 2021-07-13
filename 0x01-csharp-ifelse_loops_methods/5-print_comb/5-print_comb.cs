6882d09944d4ad6fd4056e71f256d3437a27b7e7
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 19:20:59 +0300

    Update 5-print_comb.cs

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -6,11 +6,11 @@ namespace _5_print_comb
     {
         static void Main(string[] args)
         {
-            for ( int numbers = 0; numbers <= 99 ; numbers++)
+            for (int numbers = 0; numbers <= 99; numbers++)
             {
-                if(numbers<99)
-                Console.Write("{0:d2}, ",numbers);
-                else Console.Write("{0}\n",numbers);
+                if (numbers < 99)
+                    Console.Write("{0:d2}, ", numbers);
+                else Console.Write("{0}\n", numbers);
             }
         }
     }
