5bb9dab9368e9cd9e4147709c775ca55656339ab
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 21:54:19 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printDiagonal(3);
+            printDiagonal(0);
+            printDiagonal(12);
+            printDiagonal(-98);
+        }
+
+        public static void printDiagonal(int length)
+        {
+            if (length > 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (i == j)
+                        {
+                            Console.Write("\\");
+                        }
+                        else Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else Console.WriteLine();
+        }
+    }
+}
