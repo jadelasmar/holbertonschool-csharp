9cea9b96ac1afb50b7c7bf1888273d579c685592
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 19:20:05 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( int numbers = 0; numbers <= 99 ; numbers++)
+            {
+                if(numbers<99)
+                Console.Write("{0:d2}, ",numbers);
+                else Console.Write("{0}\n",numbers);
+            }
+        }
+    }
+}
