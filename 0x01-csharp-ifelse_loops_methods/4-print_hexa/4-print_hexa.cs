b13b7fc8a38d47d932bd23862aa21269492496c3
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-13 19:09:06 +0300

    Add files via upload

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int numbers = 0; numbers <= 98; numbers++)
+            {
+                var hex = numbers.ToString("X");
+                Console.WriteLine("{0} = 0x{1}",numbers,hex);
+            }
+        }
+    }
+}
