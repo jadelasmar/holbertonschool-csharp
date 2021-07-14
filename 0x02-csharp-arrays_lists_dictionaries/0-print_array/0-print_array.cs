b3a29ab6b85062804613be040426e3af4462caa7
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 20:09:05 +0300

    Update 0-print_array.cs

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,54 +1,51 @@
 ﻿using System;
 
-namespace _0_print_array
+class Array
 {
-    class Array
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
+    }
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
         {
-            int[] newArray;
-            newArray = Array.CreatePrint(10);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(16);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(0);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(-10);
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
 
-        public static int[] CreatePrint(int size)
+        int[] numbers = new int[size];
+
+        if (size > 0)
         {
-            if (size < 0)
+            for (int i = 0; i < size; i++)
             {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-
-            int[] numbers = new int[size];
-
-            if (size > 0)
-            {
-                for (int i = 0; i < size; i++)
+                Console.Write("{0}", i);
+                if (i < size - 1)
                 {
-                    Console.Write("{0}", i);
-                    if (i < size - 1)
-                    {
-                        Console.Write(" ");
-                    }
-                    else
-                    {
-                        Console.Write("\n");
-                    }
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("\n");
                 }
             }
-            else if (size == 0)
-            {
-                Console.Write("\n");
-            }
-            return numbers;
         }
+        else if (size == 0)
+        {
+            Console.Write("\n");
+        }
+        return numbers;
     }
 }
