d7a8ea9c24b792a0f9388c6dc36a6b15c7a5ad7a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 15:47:45 +0300

    Update 4-print_hexa.cs

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -8,7 +8,7 @@ namespace _4_print_hexa
         {
             for (int numbers = 0; numbers <= 98; numbers++)
             {
-                var hex = numbers.ToString("X");
+                var hex = numbers.ToString("x");
                 Console.WriteLine("{0} = 0x{1}",numbers,hex);
             }
         }
