73c56b5ed555db75dd77773e02cd6ce81ef2b43f
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 22:15:12 +0300

    Add files via upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        int[] updatedArray = (int[])array.Clone();
+       if (index < 0 || index >= array.Length)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        updatedArray[index] = n;
+        return updatedArray;
+    }
+}
