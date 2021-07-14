8a85e65d98abb0bc8cffdc55a730067331563df2
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 22:42:53 +0300

    Update 3-print_array_reverse.cs

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,6 +1,18 @@
 ﻿using System;
 class Array
 {
+    static void Main(string[] args)
+    {
+        int[] array1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] array2 = null;
+        int[] array3 = { };
+        int[] array4 = { 98, -10, 0, 972, -42 };
+
+        Array.Reverse(array1);
+        Array.Reverse(array2);
+        Array.Reverse(array3);
+        Array.Reverse(array4);
+    }
     public static void Reverse(int[] array)
     {
         if (array == null)
@@ -10,12 +22,12 @@ class Array
         }
         for (int i = array.Length - 1; i >= 0; i--)
         {
-            if (i == 0)
+            Console.Write(array[i]);
+            if (i != 0)
             {
-                Console.WriteLine(array[i]);
+                Console.Write(" ");
             }
-            Console.Write("{0} ",array[i]);
         }
-        
+        Console.WriteLine();
     }
 }
