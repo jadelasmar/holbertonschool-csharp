dba993adc9ff44ff129f273373e7eb1aa61cc99d
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 20:25:36 +0300

    Update 0-print_array.cs

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,53 +1,49 @@
 ﻿using System;
-
-namespace _0_print_array
+class Array
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int[] newArray;
+        int[] newArray;
 
-            newArray = cPrint(10);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = cPrint(16);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = cPrint(0);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = cPrint(-10);
+        newArray = cPrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = cPrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = cPrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = cPrint(-10);
+    }
+
+    public static int[] cPrint(int siz)
+    {
+        if (siz < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
-
-        public static int[] cPrint(int siz)
+        int[] numb = new int[siz];
+        if (siz == 0)
         {
-            if (siz < 0)
+            Console.WriteLine();
+        }
+        else
+        {
+            for (int i = 0; i < siz; i++)
             {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            int[] numb = new int[siz];
-            if (siz == 0)
-            {
-                Console.WriteLine();
-            }
-            else
-            {
-                for (int i = 0; i < siz; i++)
+                Console.Write("{0}", i);
+                if (i < siz - 1)
                 {
-                    Console.Write("{0}", i);
-                    if (i < siz - 1)
-                    {
-                        Console.Write(" ");
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                    }
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.WriteLine();
                 }
             }
-            return numb;
         }
+        return numb;
     }
 }
