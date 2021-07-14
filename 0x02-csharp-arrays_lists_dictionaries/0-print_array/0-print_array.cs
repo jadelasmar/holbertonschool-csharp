0edc03b6f6b73520d9cf90aac07551419bbac443
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 19:09:15 +0300

    Add files via upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] newArray;
+            newArray = Array.CreatePrint(10);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(16);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(0);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(-10);
+        }
+
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] newArr = new int[size];
+            if (size == 0)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < size - 1; i++)
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.WriteLine(size - 1);
+            }
+            return newArr;
+        }
+    }
+}
