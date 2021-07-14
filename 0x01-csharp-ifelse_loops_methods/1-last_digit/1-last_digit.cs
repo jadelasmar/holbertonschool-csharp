1231fbe8db9f85250942189cda225e01e4933149
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 15:45:59 +0300

    Update 1-last_digit.cs

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -15,7 +15,7 @@ class Program
         {
            Console.WriteLine("The last digit of {0} is {1} and is 0", number,lastDigit);
         }
-        else Console.WriteLine("The last digit of {0} is {1} and is less than 6 ad not 0", number,lastDigit);
+        else Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number,lastDigit);
 	    
     }
 }
