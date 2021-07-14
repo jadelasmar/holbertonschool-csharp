2f3d512e4b37809f37f1e317aad8368747b93d25
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 15:52:42 +0300

    Update 6-print_comb2.cs

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -7,16 +7,10 @@ namespace _6_print_comb2
         static void Main(string[] args)
         {
             for (int i = 0; i < 9; i++)
-            {
                 for (int j = i + 1; j <= 9; j++)
-                {
                     if (i == 8 && j == 9)
-                    {
                         Console.WriteLine("{0}{1}", i, j);
-                    }
                     else Console.Write("{0}{1}, ", i, j);
-                }
-            }
         }
     }
 }
