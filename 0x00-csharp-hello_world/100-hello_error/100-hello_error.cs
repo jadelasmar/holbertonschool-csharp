7b9ec9cddcd1a8bb6806a475b7fa2cc1feec720d
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 14:28:41 +0300

    Update 100-hello_error.cs

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -7,7 +7,7 @@ namespace _100_hello_error
         static void Main(string[] args)
         {
             Environment.ExitCode = 1;
-            Console.Error.WriteLine(" and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
         }
     }
 }
