8b5645efffd5df1c88a07a7977755db03bbf7dfa
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 19:15:26 +0300

    Update 0-print_array.cs

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,21 +4,6 @@ namespace _0_print_array
 {
     class Array
     {
-        static void Main(string[] args)
-        {
-            int[] newArray;
-            newArray = Array.CreatePrint(10);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(16);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(0);
-            Console.WriteLine("Array Length: " + newArray.Length);
-            Console.WriteLine("----------------");
-            newArray = Array.CreatePrint(-10);
-        }
-
         public static int[] CreatePrint(int size)
         {
             if (size < 0)
