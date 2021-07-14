df39b6c8a448f6cc519806b021fef06db95da1e7
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 19:58:09 +0300

    Update 0-print_array.cs

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,6 +4,21 @@ namespace _0_print_array
 {
     class Array
     {
+        static void Main(string[] args)
+        {
+            int[] newArray;
+            newArray = Array.CreatePrint(10);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(16);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(0);
+            Console.WriteLine("Array Length: " + newArray.Length);
+            Console.WriteLine("----------------");
+            newArray = Array.CreatePrint(-10);
+        }
+
         public static int[] CreatePrint(int size)
         {
             if (size < 0)
@@ -11,27 +26,29 @@ namespace _0_print_array
                 Console.WriteLine("Size cannot be negative");
                 return null;
             }
-            int[] newArr = new int[size];
-            if (size == 0)
-            {
-                Console.WriteLine();
-            }
-            else
+
+            int[] numbers = new int[size];
+
+            if (size > 0)
             {
                 for (int i = 0; i < size; i++)
                 {
-                    if (i == size - 1)
+                    Console.Write("{0}", i);
+                    if (i < size - 1)
                     {
-                        Console.WriteLine("{0}", i);
+                        Console.Write(" ");
                     }
                     else
                     {
-                        Console.Write("{0} ", i);
+                        Console.Write("\n");
                     }
                 }
-
             }
-            return newArr;
+            else if (size == 0)
+            {
+                Console.Write("\n");
+            }
+            return numbers;
         }
     }
 }
