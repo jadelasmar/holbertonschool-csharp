fa7bd94e9378f911d95774e7a1329f91830118b1
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 22:33:01 +0300

    Add files via upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    public static void Reverse(int[] array)
+    {
+        int[] temp = new int[array];
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.Write(array[i]);
+        }
+        if (array == null)
+        {
+            Console.WriteLine();
+        }
+    }
+}
