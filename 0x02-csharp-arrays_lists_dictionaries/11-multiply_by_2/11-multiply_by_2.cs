5a96672ecd185562967278070d8b6595bf843639
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 23:39:14 +0300

    Add files via upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionart
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        foreach (KeyValuePair<string, int> kvp in myDict)
+        {
+            myDict[kvp.Key] = kvp.Value * 2;
+        }
+        return myDict;
+    }
+}
