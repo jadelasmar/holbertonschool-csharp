f40a21a13717791bd37b7f6b4e7e1211783a8646
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-14 23:52:12 +0300

    Add files via upload

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        List<int> value = new List<int>(myDict.Values);
+        int big=0;
+        foreach(int element in myList)
+        {
+            if(big<element)
+            {
+                big = element;
+            }
+        }
+        return big;
+    }
+}
