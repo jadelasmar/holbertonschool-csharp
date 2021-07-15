8e0927ce947d7407b21b856917bacf2f17e93caf
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 16:14:20 +0300

    commit

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,24 +5,24 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-       int number = aQueue.Count;
-       Console.WriteLine("Number of items: {0}",number);
-       bool contain = aQueue.Contains(search);
-       if (number == 0)
+        int number = aQueue.Count;
+        Console.WriteLine("Number of items: {0}", number);
+        string item = aQueue.Peek();
+        if (number > 0)
+        {
+            Console.WriteLine("First item: {0}", item);
+        }
+        else
         {
             Console.WriteLine("Queue is empty");
-            aQueue.Enqueue(newItem);
-            Console.WriteLine("Queue contains \"{0}\": {1}", search, contain);
-            return aQueue;
         }
-        string item = aQueue.Peek();
-        Console.WriteLine("First item: {0}", item);
         aQueue.Enqueue(newItem);
+        bool contain = aQueue.Contains(search);
+        Console.WriteLine("Queue contains {0}: {1}", search, contain);
         Stack<string> copy = new Stack<string>(aQueue);
+        string remove = String.Empty;
         if (contain == true)
         {
-            string remove = String.Empty;
-            Console.WriteLine("Queue contains \"{0}\": {1}", search, contain);
             foreach (string element in copy)
             {
                 remove = aQueue.Dequeue();
