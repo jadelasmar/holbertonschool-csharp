17377c83a57e6b48af37c29f4c6d7e303ebc52d2
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 10:16:24 +0300

    Update 0-unique_add.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -5,8 +5,9 @@ class List
 {
     public static int Sum(List<int> myList)
     {
+        HashSet<int> set = new HashSet<int>(myList);
         int sum = 0;
-        foreach (int element in myList)
+        foreach (int element in set)
         {
             sum += element;
         }
