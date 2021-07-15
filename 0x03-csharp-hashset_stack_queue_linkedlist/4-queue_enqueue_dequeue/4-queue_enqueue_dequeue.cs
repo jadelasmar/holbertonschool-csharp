9a50f87de6d97a598fc62dd3fb55701ca8c68f27
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 16:24:29 +0300

    commit

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,26 +5,25 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-         Console.WriteLine("Number of items: {0}", aQueue.Count);
-            if(aQueue.Count > 0)
-                {
-                    Console.WriteLine("First item: {0}", aQueue.Peek());
-                }
-            else
-                {
-                    Console.WriteLine("Queue is empty");
-                }
-            if(aQueue.Contains(search))
-                {
-                    Console.WriteLine("Queue contains \"{0}\": True", search);
-                    while((aQueue.Peek() != search || aQueue.Peek() == search) && aQueue.Contains(search))
-                    aQueue.Dequeue();
-                }
-            else
-                {
-                    Console.WriteLine("Queue contains \"{0}\": False", search);
-                }
-            aQueue.Enqueue(newItem);
-            return(aQueue);
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count > 0)
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        aQueue.Enqueue(newItem);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search) ? "True" : "False");
+        if (aQueue.Contains(search))
+        {
+            aQueue.Dequeue();
+            do
+            {
+                aQueue.Dequeue();
+            } while (aQueue.Dequeue() != search);
+        }
+        return (aQueue);
     }
 }
