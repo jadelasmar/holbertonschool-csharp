dc3b92c6659e456f95ee5fe1025da8efeb3a6cff
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 14:42:11 +0300

    commit

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -6,6 +6,23 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        foreach(int element)
+        int count = myLList.Count;
+        if (myLList.Count == 0)
+        {
+            return myLList.AddFirst(n);
+        }
+
+
+        LinkedListNode<int> node = myLList.First;
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        for (int i = 0; i < count; i++)
+        {
+            if (newNode.Value < node.Value)
+            {
+                myLList.AddBefore(node, n);
+            }
+            node = node.Next;
+        }
+        return newNode;
     }
 }
