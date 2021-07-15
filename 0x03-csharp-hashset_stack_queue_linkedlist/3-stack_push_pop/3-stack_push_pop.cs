ae9915afc4b863d83a9c32d4706b898c2636be29
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 10:38:23 +0300

    commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
