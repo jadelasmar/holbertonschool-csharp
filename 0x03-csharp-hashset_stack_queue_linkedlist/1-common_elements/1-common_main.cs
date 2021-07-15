019af4e002c0f70f0ef95a3161e0b9526f14f999
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 10:28:13 +0300

    Update 1-common_main.cs

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_main.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_main.cs
@@ -6,10 +6,13 @@ class Program
     // Main - entry point
     static void Main(string[] args)
     {
-        List<int> myList1 = new List<int>() { 1, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8 };
-        List<int> myList2 = new List<int>() { 98, 98, -14, 972, 0, -42, -972 };
+        List<int> myList1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        List<int> myList2 = new List<int>() { 0, 2, 4, 6, 7, 8 };
+        List<int> newList = new List<int>();
 
-        Console.WriteLine(List.Sum(myList1));
-        Console.WriteLine(List.Sum(myList2));
+        newList = List.CommonElements(myList1, myList2);
+
+        foreach (int i in newList)
+            Console.WriteLine(i);
     }
 }
