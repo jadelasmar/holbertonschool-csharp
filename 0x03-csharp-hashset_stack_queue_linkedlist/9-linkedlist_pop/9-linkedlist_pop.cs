0efc3bce9ea5c89017c2c168f496bf3af7397730
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 13:53:49 +0300

    commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int delete=myLList.First.Value;
+        myLList.RemoveFirst();
+        return delete;
+    }
+}
