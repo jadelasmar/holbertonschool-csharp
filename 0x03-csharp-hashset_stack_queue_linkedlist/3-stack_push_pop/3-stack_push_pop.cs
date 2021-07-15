24b1fb1f560fdc8c28ccbfa487fae4742db1f6f8
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 11:05:39 +0300

    commit

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
 
     {
-        Console.WriteLine("Hello World!");
+        int number = aStack.Count;
+        Console.WriteLine("Number of items: {0}", number);
+        if (number == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        string item = aStack.Peek();
+        Console.WriteLine("Top item: {0}", item);
+        bool contain = aStack.Contains(search);
+        Stack<string> copy = new Stack<string>(aStack);
+        if (contain == true)
+        {
+            string remove = String.Empty;
+            Console.WriteLine("Stack contains {0}: {1}", search, contain);
+            foreach (string element in copy)
+            {
+                remove = aStack.Pop();
+                if (remove == search)
+                {
+                    break;
+                }
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
     }
 }
