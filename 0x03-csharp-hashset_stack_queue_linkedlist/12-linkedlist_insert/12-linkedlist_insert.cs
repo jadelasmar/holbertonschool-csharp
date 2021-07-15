264899c10a36b39361f5caa5aa21129b730b7f3a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 14:21:53 +0300

    commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        foreach(int element)
+    }
+}
