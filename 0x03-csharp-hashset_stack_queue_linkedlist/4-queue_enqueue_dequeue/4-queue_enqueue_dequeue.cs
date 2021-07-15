ce7b711e19614be4264815199b95a72fb055ff8f
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-15 12:16:45 +0300

    commit

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _4_queue_enqueue_dequeue
+class MyQueue
 {
-    class Program
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        static void Main(string[] args)
+       int number = aQueue.Count;
+       Console.WriteLine("Number of items: {0}",number);
+       if (number == 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Queue is empty");
         }
+        string item = aQueue.Peek();
+        Console.WriteLine("Top item: {0}", item);
+        aQueue.Enqueue(newItem);
+        bool contain = aQueue.Contains(search);
+        Stack<string> copy = new Stack<string>(aQueue);
+        if (contain == true)
+        {
+            string remove = String.Empty;
+            Console.WriteLine("Queue contains \"{0}\": {1}", search, contain);
+            foreach (string element in copy)
+            {
+                remove = aQueue.Dequeue();
+                if (remove == search)
+                {
+                    break;
+                }
+            }
+        }
+        return aQueue;
     }
 }
