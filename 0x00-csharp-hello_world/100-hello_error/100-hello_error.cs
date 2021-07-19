82dd0259f7161484513f5c98adfa152824938e67
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:32:12 +0300

    [Code] Error

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace _100_hello_error
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Environment.ExitCode = 1;
-            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
-        }
+        Environment.ExitCode = 1;
+        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
     }
 }
