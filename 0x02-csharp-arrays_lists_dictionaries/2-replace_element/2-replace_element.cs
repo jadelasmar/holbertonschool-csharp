4752ed79c335569a6ed506fc07dfd2524e69700f
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:56:13 +0300

    [Method] Replace at Index

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-
 class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-       if (index < 0 || index >= array.Length)
+        if (index < 0 || index >= array.Length)
         {
             Console.WriteLine("Index out of range");
             return array;
