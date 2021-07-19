d9bee0b35280a542d2c7b92d88bd1770d412eaf0
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:48:32 +0300

    [Method] Diagonal

--- 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -1,35 +1,23 @@
 ﻿using System;
-
-namespace _11_print_diagonal
+class Program
 {
-    class Program
+    public static void printDiagonal(int length)
     {
-        static void Main(string[] args)
+        if (length > 0)
         {
-            printDiagonal(3);
-            printDiagonal(0);
-            printDiagonal(12);
-            printDiagonal(-98);
-        }
-
-        public static void printDiagonal(int length)
-        {
-            if (length > 0)
+            for (int i = 0; i < length; i++)
             {
-                for (int i = 0; i < length; i++)
+                for (int j = 0; j <= i; j++)
                 {
-                    for (int j = 0; j <= i; j++)
+                    if (i == j)
                     {
-                        if (i == j)
-                        {
-                            Console.Write("\\");
-                        }
-                        else Console.Write(" ");
+                        Console.Write("\\");
                     }
-                    Console.WriteLine();
+                    else Console.Write(" ");
                 }
+                Console.WriteLine();
             }
-            else Console.WriteLine();
         }
+        else Console.WriteLine();
     }
 }
