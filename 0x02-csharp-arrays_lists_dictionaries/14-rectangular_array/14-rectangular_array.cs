c7f9c43a3fbb657d6bc768c90f29f96e472e4986
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 19:06:30 +0300

    [Code] 5x5 2D Array

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-
-namespace _14_rectangular_array
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[,] array = new int[5, 5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
+        for (int i = 0; i < 5; i++)
         {
-            int[,] array = new int[5, 5] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } };
-            for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
             {
-                for (int j = 0; j < 5; j++)
+                Console.Write(array[i, j]);
+                if (j == 4)
                 {
-                    Console.Write(array[i, j]);
-                    if (j == 4)
-                    {
-                        Console.WriteLine();
-                    }
-                    else Console.Write(" ");
+                    Console.WriteLine();
                 }
+                else Console.Write(" ");
             }
         }
     }
