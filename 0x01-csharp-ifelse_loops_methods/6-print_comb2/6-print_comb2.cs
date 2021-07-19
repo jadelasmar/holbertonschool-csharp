6a6783ce06828d89ebbb418e8d943238fc029279
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:44:01 +0300

    [Code] All 2 Digits

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-
-namespace _6_print_comb2
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 9; i++)
-                for (int j = i + 1; j <= 9; j++)
-                    if (i == 8 && j == 9)
-                        Console.WriteLine("{0}{1}", i, j);
-                    else Console.Write("{0}{1}, ", i, j);
-        }
+        for (int i = 0; i < 9; i++)
+            for (int j = i + 1; j <= 9; j++)
+                if (i == 8 && j == 9)
+                    Console.WriteLine("{0}{1}", i, j);
+                else Console.Write("{0}{1}, ", i, j);
     }
 }
