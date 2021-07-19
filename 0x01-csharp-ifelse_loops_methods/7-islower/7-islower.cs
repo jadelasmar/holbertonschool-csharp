09f4bd11a2c952df4ce6bb8d21498a215c1f2b8b
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:45:36 +0300

    [Method] isLowercase?

--- 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,27 +1,10 @@
 ﻿using System;
-
-namespace _7_islower
+class Program
 {
-    class Program
+    public static bool isLow(char c)
     {
-        static void Main(String[] args)
-        {
-            char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g' };
-            for ( int i = 0; i < letters.Length; i++)
-            {
-                if (isLow(letters[i]))
-                {
-                    Console.WriteLine("{0} is lowercase", letters[i]);
-                }
-                else Console.WriteLine("{0} is uppercase", letters[i]);
-            }
-
-        }
-        public static bool isLow(char c)
-        {
-            if (c >= 'a' && c <= 'z')
-                return true;
-            else return false;
-        }
+        if (c >= 'a' && c <= 'z')
+            return true;
+        else return false;
     }
 }
