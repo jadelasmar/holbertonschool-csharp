46655709231f81795551087c08c4ff15d5d72c66
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:42:46 +0300

    [Code] Print to 99

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-
-namespace _5_print_comb
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for (int numbers = 0; numbers <= 99; numbers++)
         {
-            for (int numbers = 0; numbers <= 99; numbers++)
-            {
-                if (numbers < 99)
-                    Console.Write("{0:d2}, ", numbers);
-                else Console.Write("{0}\n", numbers);
-            }
+            if (numbers < 99)
+                Console.Write("{0:d2}, ", numbers);
+            else Console.Write("{0}\n", numbers);
         }
     }
 }
