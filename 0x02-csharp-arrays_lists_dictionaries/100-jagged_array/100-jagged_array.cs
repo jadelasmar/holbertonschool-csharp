980c0409d29dc2d444b0170f604997157538945a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 17:01:05 +0300

    [Code] Jagged Array

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -1,28 +1,20 @@
 ﻿using System;
-
-namespace _100_jagged_array
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[][] jaggedArray = new int[3][];
+        jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+        jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+        jaggedArray[2] = new int[] { 0, 1 };
+        for (int i = 0; i < 3; i++)
         {
-            int[][] jaggedArray = new int[3][];
-            jaggedArray[0] = new int[] { 0, 1, 2, 3 };
-            jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
-            jaggedArray[2] = new int[] { 0, 1 };
-            for (int i = 0; i < 3; i++)
+            for (int j = 0; j < jaggedArray[i].Length; j++)
             {
-                for (int j = 0; j < jaggedArray[i].Length; j++)
-                {
-                    if (j < jaggedArray[i].Length - 1)
-                    {
-                        Console.Write("{0} ", jaggedArray[i][j]);
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0}", jaggedArray[i][j]);
-                    }
-                }
+                if (j < jaggedArray[i].Length - 1)
+                    Console.Write("{0} ", jaggedArray[i][j]);
+                else
+                    Console.WriteLine("{0}", jaggedArray[i][j]);
             }
         }
     }
