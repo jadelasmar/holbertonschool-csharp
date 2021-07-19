c83e2d6fc19fc49d966961245717ec4590456b12
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:23:40 +0300

    [Code] Print Float

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-
 class Program
 {
-	static void Main(string[] args)
-        {
-		float number = 3.14159f;
-		Console.WriteLine("Float: {0:F2}",number);
-        }
+    static void Main(string[] args)
+    {
+        float number = 3.14159f;
+        Console.WriteLine("Float: {0:F2}", number);
+    }
 }
