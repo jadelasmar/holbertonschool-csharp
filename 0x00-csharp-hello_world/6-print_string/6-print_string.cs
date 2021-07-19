b2ad01e58f6bd595cf69d4613775ebc670d54265
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-19 18:24:02 +0300

    [Code] Print String

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         string str = "Holberton School";
-        Console.WriteLine(str+str+str);
-        Console.WriteLine(str.Substring(0,9));
+        Console.WriteLine(str + str + str);
+        Console.WriteLine(str.Substring(0, 9));
     }
 }
