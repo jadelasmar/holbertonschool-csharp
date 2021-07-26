2cbf92a2e0770780f55d2ce5ab788b169e843f81
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-26 09:37:16 +0300

    [Method] Safe Print

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -4,6 +4,21 @@ class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
+        int element = 0;
+        try
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                element++;
+            }
+            return element;
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
 
     }
 }
