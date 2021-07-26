307cb7e40d4353c17d1256b6e34b09392dd6c445
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-26 09:51:54 +0300

    [Function] Division

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -4,6 +4,15 @@ class Int
 {
     public static void divide(int a, int b)
     {
-
+        int div = 0;
+        try
+        {
+            div = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot Divide by zero");
+        }
+        finally { Console.WriteLine("{0} / {1} = {2}", a, b, div); }
     }
 }
