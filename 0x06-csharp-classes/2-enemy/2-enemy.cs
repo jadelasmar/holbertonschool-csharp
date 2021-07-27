07f5cc175fb8212d7905bec1c350e975d3621357
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-27 20:58:43 +0300

    [Class] Health Validation

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creation of public class Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Creation of int health</summary>
+        public int health;
+        ///<summary>Creation of public constructor for Zombie and Setting health to 0</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Creation of public constructor with exception</summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
