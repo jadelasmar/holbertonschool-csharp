2b1cdcd8ce89a0d9a7b92464b302dc24da24d181
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-27 21:13:37 +0300

    [Class] Get Health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creation of public class Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Creation of int health</summary>
+        private int health;
+        ///<summary>Creation of public constructor for Zombie and Setting health to 0</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Creation of public constructor with exception</summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+        }
+        ///<summary>Creation of method that return Health</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
