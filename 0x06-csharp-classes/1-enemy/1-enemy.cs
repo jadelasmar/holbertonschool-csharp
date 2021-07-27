62ed87b47d6d1059c6173d029066608fb9d895a9
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-27 12:56:46 +0300

    [Class] Healthy

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Creation of public class Zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Creation of int health</summary>
+        public int health;
+        ///<summary>Creation of public constructor for Zombie and Setting health to 0</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
