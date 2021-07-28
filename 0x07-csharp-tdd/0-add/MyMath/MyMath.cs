15947c004eb23e3fb6af82e2079a414f6312122a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-28 12:40:51 +0300

    [Test] Addition

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -3,13 +3,13 @@
 namespace MyMath
 {
     ///<summary>Creation of class Operations</summary>
-    ///<param name="a">new int a</param>
-    ///<param name="b">new int b</param>
-    ///<returns>addition of a and b</returns>
+
     public class Operations
     {
+        ///<param name="a">int a</param>
+        ///<param name="b">int b</param>
+        ///<returns>addition of a and b</returns>
         ///<summary>Method for adding two int</summary>
-
         public static int Add(int a, int b)
         {
             return a + b;
