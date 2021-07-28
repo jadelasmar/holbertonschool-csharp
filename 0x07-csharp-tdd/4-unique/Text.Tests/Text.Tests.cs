a09f51dc8c53b8659c70fde02f3f1627d24e0c24
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-28 10:37:40 +0300

    [Test] Unique

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Creation of class str</summary>
+    public class Str
+    {
+        ///<summary>find first non repeating</summary>
+        ///<param name="s">string of char</param>
+        ///<returns>index or -1 if no unique char</returns>
+        public static int UniqueChar(string s)
+        {
+            char[] arr = s.ToCharArray();
+            for(int i=0;i< x.Length; i++)
+        }
+
+    }
+}
