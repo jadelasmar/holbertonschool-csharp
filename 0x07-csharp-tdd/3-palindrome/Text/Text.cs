6e1482222e54c7dac969c755862177a57e3ad2fa
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-29 09:09:07 +0300

    [Test] Palindrome

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -16,23 +16,20 @@ namespace Text
             {
                 return true;
             }
-            else
+            ///<summary>convert to lower case and removing punctuation </summary>
+            s = s.ToLower();
+            s = Regex.Replace(s, "[:;.,'<>/?!@#$%^&*(){} ]", "");
+            char[] revS = s.ToCharArray();
+            Array.Reverse(revS);
+            ///<summary>check if s is the same with revS</summary>
+            for (int i = 0; i < s.Length; i++)
             {
-                ///<summary>convert to lower case and removing punctuation </summary>
-                s = s.ToLower();
-                s = Regex.Replace(s, "[:;.,'<>/?!@#$%^&*(){} ]", "");
-                char[] revS = s.ToCharArray();
-                Array.Reverse(revS);
-                ///<summary>check if s is the same with revS</summary>
-                for (int i = 0; i < s.Length; i++)
+                if (s[i] != revS[i])
                 {
-                    if (s[i] != revS[i])
-                    {
-                        return false;
-                    }
+                    return false;
                 }
-                return true;
             }
+            return true;
         }
     }
 }
