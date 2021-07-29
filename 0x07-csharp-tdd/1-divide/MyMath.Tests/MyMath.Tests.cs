dbf84b66e1853d24140f9e5ad3210566832a3197
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-29 09:42:39 +0300

    [Test] Division

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -3,49 +3,56 @@ using NUnit.Framework;
 namespace MyMath.Tests
 {
     [TestFixture]
-    /// <summary>Tests</summary>
-    public class MatrixTests
+    ///<summary>Start a new set of tests for Division</summary>
+    public class Tests
     {
         [Test]
-        public void evenNumbers()
+        ///<summary>odd number division</summary>
+        public void oddDiv()
         {
-
-            int[,] result = Matrix.Divide(new int[,] { { 1, 2 }, { 4, 4 }, { 6, 6 }, { 8, 8 } }, 2);
-
-            Assert.AreEqual(new int[,] { { 0, 1 }, { 2, 2 }, { 3, 3 }, { 4, 4 } }, result);
-        }
-
-        [Test]
-        public void oddNUmbers()
-        {
-            int[,] matrix = new int[,] { { 3, 24 }, { 5, 10 }, { 15, 6 }, { 9, 8 } };
+            int[,] matrix = new int[,] { { 3, 5 }, { 1, 7 }, { 9, 3 }, { 7, 1 } };
             int num = 2;
-
-            int[,] result = Matrix.Divide(matrix, num);
-
-            Assert.AreEqual(new int[,] { { 1, 12 }, { 2, 5 }, { 7, 3 }, { 4, 4 } }, result);
+            int[,] newMat = Matrix.Divide(matrix, num);
+            int[,] ans = new int[,] { { 1, 2 }, { 0, 3 }, { 4, 1 }, { 3, 0 } };
+            Assert.AreEqual(ans, newMat);
         }
-
         [Test]
-        public void numEquals0()
+        ///<summary>even number division</summary>
+        public void evenDiv()
         {
-            int[,] matrix = new int[,] { { 3, 24 }, { 5, 10 }, { 15, 6 }, { 9, 8 } };
-            int num = 0;
-
-            int[,] result = Matrix.Divide(matrix, num);
-
-            Assert.AreEqual(null, result);
+            int[,] matrix = new int[,] { { 2, 4 }, { 2, 6 }, { 8, 2 }, { 6, 8 } };
+            int num = 2;
+            int[,] newMat = Matrix.Divide(matrix, num);
+            int[,] ans = new int[,] { { 1, 2 }, { 1, 3 }, { 4, 1 }, { 3, 4 } };
+            Assert.AreEqual(ans, newMat);
         }
-
         [Test]
-        public void nullMatrix()
+        ///<summary>null matrix division</summary>
+        public void nullDiv()
         {
             int[,] matrix = null;
             int num = 2;
-
-            int[,] result = Matrix.Divide(matrix, num);
-
-            Assert.AreEqual(null, result);
+            int[,] newMat = Matrix.Divide(matrix, num);
+            Assert.AreEqual(null, newMat);
+        }
+        [Test]
+        ///<summary>division by 0</summary>
+        public void zeroDiv()
+        {
+            int[,] matrix = new int[,] { { 3, 5 }, { 1, 7 }, { 9, 3 }, { 7, 1 } };
+            int num = 0;
+            int[,] newMat = Matrix.Divide(matrix, num);
+            Assert.AreEqual(null, newMat);
+        }
+        [Test]
+        ///<summary>0 in matrix</summary>
+        public void matZeroDiv()
+        {
+            int[,] matrix = new int[,] { { 0, 5 }, { 1, 0 }, { 9, 3 }, { 0, 1 } };
+            int num = 2;
+            int[,] newMat = Matrix.Divide(matrix, num);
+            int[,] ans = new int[,] { { 0, 2 }, { 0, 0 }, { 4, 1 }, { 0, 0 } };
+            Assert.AreEqual(ans, newMat);
         }
     }
 }
--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -2,34 +2,36 @@
 
 namespace MyMath
 {
-    /// <summary>Matrix Class</summary>
+    ///<summary>Creation of class matrix</summary>
     public class Matrix
     {
-        /// <summary>Add two int Method</summary>
-        /// <param name="matrix">Array of integers.</param>
-        /// <param name="num">number to divide.</param>
-        /// <returns>a new matrix containing divided elements.</returns>
+        ///<summary>method to divide matrix</summary>
+        ///<param name="matrix">array of int</param>
+        ///<param name="num">num to divide</param>
+        ///<returns>new matrix divided</returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            if (num == 0)
+
+            if (matrix == null)
+            {
+                return (null);
+            }
+            try
+            {
+                int[,] newMat = new int[matrix.GetLength(0), matrix.GetLength(1)];
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        newMat[i, j] = matrix[i, j] / num;
+                    }
+                }
+                return (newMat);
+            }
+            catch (DivideByZeroException)
             {
                 Console.WriteLine("Num cannot be 0");
                 return (null);
-            }
-            else if (matrix == null)
-            {
-                return (null);
-            }
-            else
-            {
-                for (int x = 0; x < matrix.GetLength(0); x++)
-                {
-                    for (int y = 0; y < matrix.GetLength(1); y++)
-                    {
-                        matrix[x, y] = matrix[x, y] / num;
-                    }
-                }
-                return (matrix);
             }
         }
     }
