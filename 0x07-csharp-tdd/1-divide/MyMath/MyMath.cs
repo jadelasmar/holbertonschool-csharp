b1a54a17c28a96e958c298cd036e24a005d5c95e
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-29 08:15:46 +0300

    [Test] Division

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -11,25 +11,29 @@ namespace MyMath
         ///<returns>divided matrix by num</returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            if (num == 0)
+            int[,] newMat = matrix;
+            try
+            {
+                if (matrix == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            newMat[i, j] = matrix[i, j] / num;
+                        }
+                    }
+                    return matrix;
+                }
+            }
+            catch (DivideByZeroException)
             {
                 Console.WriteLine("Num cannot be 0");
                 return null;
-            }
-            else if (matrix == null)
-            {
-                return null;
-            }
-            else
-            {
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        matrix[i, j] = matrix[i, j] / num;
-                    }
-                }
-                return matrix;
             }
         }
     }
