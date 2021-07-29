f9584f1753d2b96f7565311bd17b6d8fa8b0ab5b
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-29 08:13:30 +0300

    [Test] Division "removed try/catch"

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -11,28 +11,25 @@ namespace MyMath
         ///<returns>divided matrix by num</returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            try
-            {
-                if (matrix == null)
-                {
-                    return null;
-                }
-                else
-                {
-                    for (int i = 0; i < matrix.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < matrix.GetLength(1); j++)
-                        {
-                            matrix[i, j] = matrix[i, j] / num;
-                        }
-                    }
-                    return matrix;
-                }
-            }
-            catch (DivideByZeroException)
+            if (num == 0)
             {
                 Console.WriteLine("Num cannot be 0");
                 return null;
+            }
+            else if (matrix == null)
+            {
+                return null;
+            }
+            else
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+                return matrix;
             }
         }
     }
