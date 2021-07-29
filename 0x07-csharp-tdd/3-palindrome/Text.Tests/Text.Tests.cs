a5fc7294dc5fa966b9f6c13dbde4daa690d8f94e
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-29 09:15:25 +0300

    [Test] Palindrome

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -2,46 +2,57 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
-    public class StrTests
+    [TestFixture]
+    ///<summary>Starting set of tests for palidnrome</summary>
+    public class Tests
     {
-
         [Test]
-		public void Main()
-		{
-			string s = "A man, a plan, a canal: Panama.";
-
-			bool result = Str.IsPalindrome(s);
-
-			Assert.AreEqual(true, result);
-		}
-
+        ///<summary>string with punctuations</summary>
+        public void punctStr()
+        {
+            string str = "A man, a plan, a canal: Panama.";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(true,tOrF);
+        }
         [Test]
-		public void threeLetterPali()
-		{
-			string s = "aba";
-
-			bool result = Str.IsPalindrome(s);
-
-			Assert.AreEqual(true, result);
-		}
-
-		[Test]
-		public void threeLetternotPali()
-		{
-			string s = "esve";
-
-			bool result = Str.IsPalindrome(s);
-
-			Assert.AreEqual(false, result);
-		}
-		[Test]
-		public void Empty()
-		{
-			string s = "";
-
-			bool result = Str.IsPalindrome(s);
-
-			Assert.AreEqual(true, result);
-		}
+        ///<summary>empty string</summary>
+        public void empStr()
+        {
+            string str = "";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(true,tOrF);
+        }
+        [Test]
+        ///<summary>palidnrome string</summary>
+        public void normStr()
+        {
+            string str = "redivider";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(true,tOrF);
+        }
+        [Test]
+        ///<summary>case sens string</summary>
+        public void capStr()
+        {
+            string str = "Level";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(true,tOrF);
+        }
+        [Test]
+        ///<summary>one char string</summary>
+        public void charStr()
+        {
+            string str = "a";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(true,tOrF);
+        }
+        [Test]
+        ///<summary>not palindrome string</summary>
+        public void noneStr()
+        {
+            string str = "visual";
+            bool tOrF = Str.IsPalindrome(str);
+            Assert.AreEqual(false,tOrF);
+        }
     }
 }
