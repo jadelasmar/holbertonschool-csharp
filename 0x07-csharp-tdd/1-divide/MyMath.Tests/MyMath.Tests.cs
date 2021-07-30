d081832b847b30a2602c94586c669ba5d6d6d8ec
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-07-30 20:27:19 +0300

    [Test] Division

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -7,43 +7,45 @@ namespace MyMath.Tests
     public class Tests
     {
         [Test]
-        ///<summary>odd number division</summary>
-        public void oddDiv()
-        {
-            int[,] matrix = new int[,] { { 3, 5 }, { 1, 7 }, { 9, 3 }, { 7, 1 } };
-            int num = 2;
-            int[,] newMat = Matrix.Divide(matrix, num);
-            int[,] ans = new int[,] { { 1, 2 }, { 0, 3 }, { 4, 1 }, { 3, 0 } };
-            Assert.AreEqual(ans, newMat);
-        }
-        [Test]
         ///<summary>even number division</summary>
         public void evenDiv()
         {
-            int[,] matrix = new int[,] { { 2, 4 }, { 2, 6 }, { 8, 2 }, { 6, 8 } };
+            int[,] matrix = new int[,] { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
             int num = 2;
             int[,] newMat = Matrix.Divide(matrix, num);
-            int[,] ans = new int[,] { { 1, 2 }, { 1, 3 }, { 4, 1 }, { 3, 4 } };
+            int[,] ans = new int[,] { { 49, 0, -6 }, { 10, 486, 22 }, { -363, 30, -1 } };
             Assert.AreEqual(ans, newMat);
         }
         [Test]
-        ///<summary>null matrix division</summary>
-        public void nullDiv()
+        ///<summary>odd number division</summary>
+        public void oddDiv()
         {
-            int[,] matrix = null;
-            int num = 2;
+            int[,] matrix = new int[,] { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
+            int num = 7;
             int[,] newMat = Matrix.Divide(matrix, num);
-            Assert.AreEqual(null, newMat);
+            int[,] ans = new int[,] { { 14, 0, -1 }, { 3, 138, 6 }, { -103, 8, -0 } };
+            Assert.AreEqual(ans, newMat);
         }
         [Test]
         ///<summary>division by 0</summary>
         public void zeroDiv()
         {
-            int[,] matrix = new int[,] { { 3, 5 }, { 1, 7 }, { 9, 3 }, { 7, 1 } };
+            int[,] matrix = new int[,] { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
             int num = 0;
             int[,] newMat = Matrix.Divide(matrix, num);
             Assert.AreEqual(null, newMat);
         }
+
+        [Test]
+        ///<summary>null matrix division</summary>
+        public void nullDiv()
+        {
+            int[,] matrix = null;
+            int num = 4;
+            int[,] newMat = Matrix.Divide(matrix, num);
+            Assert.AreEqual(null, newMat);
+        }
+
         [Test]
         ///<summary>0 in matrix</summary>
         public void matZeroDiv()
