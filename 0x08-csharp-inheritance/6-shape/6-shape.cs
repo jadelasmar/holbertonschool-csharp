6e04c3bbd65c1449417f1dfa54749e64ea1b120e
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-02 11:36:58 +0300

    [Class] class with publicProperties

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,54 @@
+using System;
+/// <summary>
+/// new class shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// public virtual method
+    /// </summary>
+    /// <returns>string</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else { height = value; }
+        }
+}
+
+}
