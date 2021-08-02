b9ffbdfd3d64c25b1d25c0a99a8c7be7e5da86cd
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-02 10:43:20 +0300

    [Method] Obj isInt

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,16 @@
 using System;
 using System.Collections.Generic;
 
+/// <summary>
+///new class obj
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// method to check if obj is an int
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns>true/false</returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int)
