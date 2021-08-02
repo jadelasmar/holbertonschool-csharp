2605c6b454a81f660604c215bd5b5ab7d286c683
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-02 11:24:36 +0300

    [Class] Subclass

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,16 @@
+using System;
+/// <summary>
+/// new class animal
+/// </summary>
+class Animal
+{
+
+}
+
+/// <summary>
+/// new subclass dog from class animal
+/// </summary>
+class Dog : Animal
+{
+
+}
