deb37dc432a0d600db4b199cfd952ba8191c01d5
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-02 11:29:18 +0300

    [Class] publicMethod with throwException

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+using System;
+/// <summary>
+/// new class shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// public virtual method
+    /// </summary>
+    /// <returns>string</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
