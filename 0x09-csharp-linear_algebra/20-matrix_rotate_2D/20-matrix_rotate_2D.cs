7069c59f267267adf07084e04f9c4d769170784a
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-22 09:55:09 +0300

    test

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -12,23 +12,30 @@ class MatrixMath
     /// <returns>new matrix</returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
-        {
-            double[,] mat = new double[2, 2];
-            double cos = Math.Cos(angle);
-            double sin = Math.Sin(angle);
-            mat[0, 0] = Math.Round((cos * matrix[0, 0]), 2) - Math.Round((sin * matrix[0, 1]), 2);
-            mat[0, 1] = Math.Round((sin * matrix[0, 0]), 2) + Math.Round((cos * matrix[0, 1]), 2);
-            mat[1, 0] = Math.Round((cos * matrix[1, 0]), 2) - Math.Round((sin * matrix[1, 1]), 2);
-            mat[1, 1] = Math.Round((sin * matrix[1, 0]), 2) + Math.Round((cos * matrix[1, 1]), 2);
-            return mat;
-        }
+        double[,] fail = { { -1 } };
 
-        else
-        {
-            double[,] min1 = { { -1 } };
-            return min1;
-        }
+        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return (fail);
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return (fail);
+
+        double[,] total = new double[2, 2];
+
+        double x1 = matrix[0, 0];
+        double y1 = matrix[0, 1];
+        double x2 = matrix[1, 0];
+        double y2 = matrix[1, 1];
+
+        double cos = Math.Cos(angle);
+        double sin = Math.Sin(angle);
+
+        total[0, 0] = Math.Round((x1 * cos), 2) - Math.Round((y1 * sin), 2);
+        total[0, 1] = Math.Round((x1 * sin), 2) + Math.Round((y1 * cos), 2);
+        total[1, 0] = Math.Round((x2 * cos), 2) - Math.Round((y2 * sin), 2);
+        total[1, 1] = Math.Round((x2 * sin), 2) + Math.Round((y2 * cos), 2);
+
+        return (total);
 
     }
 }
