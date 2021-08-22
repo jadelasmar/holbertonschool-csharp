fc13e82ad929634068078fb368efc5901daea26d
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-08-22 10:08:03 +0300

    test

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -12,35 +12,23 @@ class MatrixMath
     /// <returns>new matrix</returns>
     public static double[,] Rotate2D(double[,] matrix, double angle)
     {
-        double cos = Math.Cos(angle);
-        double sin = Math.Sin(angle);
-        double[,] rotateMatrix = new double[,] {
-            {cos, sin},
-            {-sin, cos}
-        };
-
         if (matrix is double[,] && matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            int rowM1 = matrix.GetLength(0);
-            int colM1 = matrix.GetLength(1);
-            int colM2 = rotateMatrix.GetLength(1);
-            int rowM2 = rotateMatrix.GetLength(0);
-
-            double[,] mulMatrix = new double[rowM1, colM2];
-
-            for (int col = 0; col < colM1; col++)
-            {
-                for (int row = 0; row < rowM1; row++)
-                {
-                    for (int ixj = 0; ixj < colM2; ixj++)
-                    {
-                        mulMatrix[row, ixj] = Math.Round(mulMatrix[row, ixj] + matrix[row, col] * rotateMatrix[col, ixj], 2);
-                    }
-                }
-            }
-            return mulMatrix;
+            double[,] mat = new double[2, 2];
+            double cos = Math.Cos(angle);
+            double sin = Math.Sin(angle);
+            mat[0, 0] = Math.Round((cos * matrix[0, 0]), 2) - Math.Round((sin * matrix[0, 1]), 2);
+            mat[0, 1] = Math.Round((sin * matrix[0, 0]), 2) + Math.Round((cos * matrix[0, 1]), 2);
+            mat[1, 0] = Math.Round((cos * matrix[1, 0]), 2) - Math.Round((sin * matrix[1, 1]), 2);
+            mat[1, 1] = Math.Round((sin * matrix[1, 0]), 2) + Math.Round((cos * matrix[1, 1]), 2);
+            return mat;
         }
-        else { return new double[,] { { -1 } }; }
+
+        else
+        {
+            double[,] min1 = { { -1 } };
+            return min1;
+        }
 
     }
 }
