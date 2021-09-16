0037e3339e5d2ea9be15cff1355166aecaca6114
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-09-16 18:07:30 +0300

    add all methods

--- image_processor/main.cs
+++ image_processor/main.cs
@@ -12,6 +12,6 @@ class Program
         else
             filenames = Directory.GetFiles("images/", "*.jpg");
 
-        ImageProcessor.Thumbnail(filenames,5);
+        ImageProcessor.Inverse(filenames);
     }
 }
