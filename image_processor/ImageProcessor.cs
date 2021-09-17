20fea7bbda1b581d87065c4ae465b398fccce558
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-09-17 09:06:00 +0300

    test4

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 /// <summary>
 /// new class ImageProcessor
