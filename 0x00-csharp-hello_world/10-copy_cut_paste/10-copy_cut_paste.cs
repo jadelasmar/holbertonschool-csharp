5981b049cfcc8447ffb268cc8182aba0d41db06c
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-09-23 15:21:58 +0300

    change holberton to school

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        String word = "Holberton";
+        String word = "School";
         String word_first_3 = word.Substring(0, 3);
         String word_last_2 = word.Substring(word.Length - 2, 2);
         String middle_word = word.Substring(1, word.Length - 2);
