a30f2f451d872a9bbaed741740965ea7ec984f12
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-11-06 19:43:14 +0200

    [Class] Abstract - Base

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// new abstract class
+/// </summary>
+abstract class Base
+{
+    public string name;
+    //override of ToString()
+    public override string ToString()
+    {
+        return name + " is a " + GetType().Name;
+    }
+}
