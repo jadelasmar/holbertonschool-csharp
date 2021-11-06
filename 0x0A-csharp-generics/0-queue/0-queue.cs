e89ff3aa9a4a068ffe53bc4a0f07cf9a8468d8f6
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-11-06 10:56:23 +0200

    [Method] Queue - CheckType

---
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0_queue
+{
+    /// <summary>
+    /// generic queue class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class Queue<T>
+    {
+        /// <summary>
+        /// method to get type
+        /// </summary>
+        /// <returns></returns>
+        public Type CheckType()
+        {
+            return typeof(T);
+        }
+    }
+}
