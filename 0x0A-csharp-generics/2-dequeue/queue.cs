53bfdc1f34cde157b74e1528ee2c75d997e18bb2
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-11-06 14:45:58 +0200

    [Method] Dequeue - remove head & return value

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,85 @@
+using System;
+
+/// <summary>
+/// generic queue class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    /// <summary>
+    /// method to get type
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// new class Node
+    /// </summary>
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+
+    /// <summary>
+    /// add new node
+    /// </summary>
+    public void Enqueue(T value)
+    {
+        Node node = new Node(value);
+
+        //if head null, head is the new node
+        if (head == null)
+            head = node;
+        else
+            tail.next = node;
+        tail = node;
+        count++;
+    }
+
+    /// <summary>
+    /// get number of nodes
+    /// </summary>
+    /// <returns>count</returns>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// remove head and return value
+    /// </summary>
+    /// <returns></returns>
+    public T Dequeue()
+    {
+        //return head if not empty
+        if (head != null)
+        {
+            T value = head.value;
+            head = head.next;
+            count--;
+            return value;
+        }
+        //if node empty
+        else
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+    }
+}
