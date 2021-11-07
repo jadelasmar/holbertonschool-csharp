d070cd72ea29daf05d541e6f8dd5760fd342af2e
Jad ElAsmar <el.asmar.jad.94@gmail.com>
2021-11-07 08:24:17 +0200

    [Class] Player - PrintHealth

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -5,9 +5,9 @@ using System;
 /// </summary>
 public class Player
 {
-    string name;
-    float maxHp;
-    float hp;
+    string name { set; get; }
+    float maxHp { set; get; }
+    float hp { set; get; }
     /// <summary>
     /// constructor
     /// </summary>
@@ -16,19 +16,19 @@ public class Player
     public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
-        if (maxHp > 0) {
-            this.maxHp = maxHp;
-        } else {
-            this.maxHp = 100f;
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
-        this.hp = this.maxHp;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
     }
     /// <summary>
     /// method to print health
     /// </summary>
     public void PrintHealth()
     {
-        Console.WriteLine("{0} has {1}/{2} health",name,hp,maxHp);
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
     }
 }
